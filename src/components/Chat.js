ff6ffaf8c6dda78aeda1cc2f268860b945281397
EC2 Default User <ec2-user@ip-172-31-18-191.us-east-2.compute.internal>
2020-01-15 20:36:23 +0000

    Redirect to /chat upon group form submission

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const Chat = () => {
+const Chat = (props) => {
+    const user = props.location.state.user;
+    console.log(user);
+    
     return (
-        <h1>Chat</h1>
+        <h1 style={{color: 'white'}}>Hello '{user.username}'! You are in the '{user.groupID}' group.</h1>
     );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,41 +1,46 @@
 import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom'
 
 const Form = () => {
-    const [ username, setUsername ] = useState('');
-    const [ groupID, setGroupID ] = useState('');
+    const [ user, setUser ] = useState({username: '', groupID: ''});
+    const [ redirect, setRedirect ] = useState(false);
+    
     
     const handleChange = (e) => {
         const value = e.target.value;
         
         if (e.target.id === 'username-input') {
-            setUsername(value);
+            setUser({username: value, groupID: user.groupID});
         } else {
-            setGroupID(value);
+            setUser({username: user.username, groupID: value});
         }
     }
     
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Submitted: ' + username + ' ' + groupID);
+        setRedirect(true);
     }
     
-    
-    return (
-        <div>
-            <form id='group-form' onSubmit={handleSubmit} onChange={handleChange}>
-                <div class="form-group">
-                    <label for="username-input">Username</label>
-                    <input type="text" class="form-control" id="username-input" placeholder="Username"/>
-                    <small class="form-text text-muted">This is how others will identify you.</small>
-                </div>
-                <div class="form-group">
-                    <label for="group-input">Group ID</label>
-                    <input type="text" class="form-control" id="group-input" placeholder="Group ID"/>
-                    <small class="form-text text-muted">Group to create or join if it already exists.</small> 
-                </div>
-            </form>
-        </div>
-    );
+    return redirect ? <Redirect to={{
+            pathname: '/chat',
+            state: { user }
+        }} /> :
+        (
+            <div>
+                <form id='group-form' onSubmit={handleSubmit} onChange={handleChange}>
+                    <div class="form-group">
+                        <label for="username-input">Username</label>
+                        <input type="text" class="form-control" id="username-input" placeholder="Username"/>
+                        <small class="form-text text-muted">This is how others will identify you.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="group-input">Group ID</label>
+                        <input type="text" class="form-control" id="group-input" placeholder="Group ID"/>
+                        <small class="form-text text-muted">Group to create or join if it already exists.</small> 
+                    </div>
+                </form>
+            </div>
+        );
 }
 
 export default Form;
