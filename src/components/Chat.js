90b0a8502b4b248f65c1c7923332a94a518762ef
EC2 Default User <ec2-user@ip-172-31-18-191.us-east-2.compute.internal>
2020-01-16 04:41:31 +0000

    Created Chat prop, no functionality yet

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const Chat = () => {
-    <div>
-        
+const Chat = (props) => {
+    const group = props.group;
     
-    </div>
-    
+    return (
+        <div id='chat-container' className='d-flex flex-column h-100' style={{backgroundColor: '#252526'}}>
+            <div className='text-center' style={{borderBottom: 'solid 1px #E53A3A'}}>
+                <b>{group}</b> Chatroom
+            </div>
+            
+            <div id='chat-area' className='' style={{height: '90%'}}>
+                
+            </div>
+            <div id='chat-input-container' className='d-flex flex-column'>
+                <div> 
+                    <hr className='mb-0' style={{backgroundColor: '#C4C4C4'}}/>
+                </div>
+                
+                <div className='d-flex align-items-center mt-2'>
+                    <div id='chat-input' className='flex-grow-1 ml-2'>
+                        <input type="text" placeholder="Say something..." style={{width: '95%'}}/>
+                    </div>
+                    <div className='mr-2 mb-2'>
+                        <button id='chat-send' class='btn btn-primary'>Send</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
+
+export default Chat;
--- src/components/Room.js
+++ src/components/Room.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import io from 'socket.io-client';
 
+import Chat from './Chat';
+
 const SERVER_URL = 'https://58aab3c90017465bbb8c7cbf0b87d6b3.vfs.cloud9.us-east-2.amazonaws.com';
 const SERVER_PORT = '8081';
 const SERVER_ENDPOINT = SERVER_URL.concat(':', SERVER_PORT);
@@ -12,11 +14,23 @@ const Room = (props) => {
     console.log(socket);
     
     return (
-        <div style={{color: 'white'}}>
-            <nav class="navbar navbar-dark bg-dark">
-                <a class="navbar-brand" href="/">Lets<span style={{color: '#E53A3A'}}>Watch</span></a>
-                <span>{user.username}</span>
-            </nav>
+        <div className="container-fluid m-auto h-100" style={{color: 'white'}}>
+            <div className='row'>
+                <nav className="navbar navbar-dark bg-dark w-100">
+                    <a className="navbar-brand" href="/">Lets<span style={{color: '#E53A3A'}}>Watch</span></a>
+                    <span>{user.username}</span>
+                </nav>
+            </div>
+            
+            <div className='row h-50 p-3'>
+                <div className='col-8' style={{backgroundColor: '#E53A3A'}}>
+                    Video goes here
+                </div>
+                
+                <div className='col pr-0'>
+                    <Chat group={user.groupID}/>
+                </div>
+            </div>
         </div> 
           
         // <h1 style={{color: 'white'}}>Hello '{user.username}'! You are in the '{user.groupID}' group.</h1>
