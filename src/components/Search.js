59cb2522f4c990fd2498b99d7f7f5dcf0ea83567
EC2 Default User <ec2-user@ip-172-31-18-191.us-east-2.compute.internal>
2020-01-20 07:28:09 +0000

    Implemented youtube data api query

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,19 @@
 import React, {useEffect} from 'react';
+import fetch from 'node-fetch';
 
 
 const Search = () => {
     
-    const useEffect = () => {
-        fetch('http://example.com/movies.json')
+    useEffect(() => {
+        console.log('fetching youtube search');
+        fetch('https://www.googleapis.com/youtube/v3/search?&key=AIzaSyBa-JzGFfw19oswz7L6WV0BwbNMBIZw5Ko&part=snippet&q=kpop&maxResults=25')
           .then((response) => {
             return response.json();
           })
           .then((myJson) => {
             console.log(myJson);
           });
-    }
+    }, []);
 
     return (
         <div className='h-100 w-100' style={{backgroundColor:'blue'}}>
