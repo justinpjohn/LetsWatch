86e2531f900ba413885c3e713d300ef5a6abd134
EC2 Default User <ec2-user@ip-172-31-18-191.us-east-2.compute.internal>
2020-01-20 07:02:18 +0000

    installed 'node-fetch' package

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 
 const Search = () => {
+    
+    const useEffect = () => {
+        fetch('http://example.com/movies.json')
+          .then((response) => {
+            return response.json();
+          })
+          .then((myJson) => {
+            console.log(myJson);
+          });
+    }
 
     return (
         <div className='h-100 w-100' style={{backgroundColor:'blue'}}>
