e637ba3874ea5162929f0d7daba8b5d8a94ecacd
EC2 Default User <ec2-user@ip-172-31-18-191.us-east-2.compute.internal>
2020-01-23 04:55:23 +0000

    playing with configs

--- server/index.js
+++ server/index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 const cors = require('cors');
+const path = require('path');
 
 const PORT = process.env.PORT || 8081;
 // const PORT = 8081;
 
 const router = require('./router');
 
-const app = express();
+const INDEX = '../public/';
+
+const app = express().use(express.static(__dirname + '/../build'));
 const server = http.createServer(app);
 const io = socketio(server);
 
@@ -54,7 +57,7 @@ io.on('connection', (socket) => {
 });
 
 
-app.use(router);
+// app.use(router);
 app.use(cors());
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
