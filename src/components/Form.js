9c44e61bb57b386d99264ae3c8cd0f786c1ed3fa
Justin <justinj9897@tamu.edu>
2020-09-13 01:21:27 +0000

    Added character limit to group form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,6 +4,9 @@ import { Redirect } from 'react-router-dom'
 
 const Form = () => {
     
+    const MAX_USERNAME_LENGTH = 30;
+    const MAX_GROUPID_LENGTH = 30;
+    
     const [ userData, setUserData ] = useState({userName: '', roomName: ''});
     const [ redirect, setRedirect ] = useState(false);
     
@@ -31,12 +34,12 @@ const Form = () => {
                 <form id='group-form' onSubmit={handleSubmit} onChange={handleChange}>
                     <div class="form-group">
                         <label for="username-input">Username</label>
-                        <input type="text" class="form-control" id="username-input" placeholder="Username"/>
+                        <input type="text" class="form-control" id="username-input" placeholder="Username" maxlength = { MAX_USERNAME_LENGTH }/>
                         <small class="form-text text-muted">This is how others will identify you.</small>
                     </div>
                     <div class="form-group">
                         <label for="group-input">Group ID</label>
-                        <input type="text" class="form-control" id="group-input" placeholder="Group ID"/>
+                        <input type="text" class="form-control" id="group-input" placeholder="Group ID" maxlength = { MAX_GROUPID_LENGTH }/>
                         <small class="form-text text-muted">Group to create or join if it already exists.</small> 
                     </div>
                 </form>
