13f6e7861b63c65689bca4df8f1214e0d0dd0596
Justin <justinj9897@tamu.edu>
2020-09-14 22:51:44 +0000

    fixed home screen for mobile devices

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,19 +9,17 @@ const Home = () => {
     const { isVisible, toggle } = useModal();
 
     return (
-        <div className='container d-flex h-100'>
-            <div className='row justify-content-center align-self-center flex-column w-100 h-75'>
-                <div className='row justify-content-center mb-5' style={{fontSize: '5.5em'}}> 
-                    <span style={{color: 'white'}}>Lets</span>
-                    <span style={{color: '#E53A3A'}}>Watch</span>
-                </div>
-                
-                <div className='row justify-content-center mt-3'>
-                    <button className='main-button' onClick={ toggle } style={{backgroundColor: '#E53A3A', borderColor: '#E53A3A', color: 'white'}}> GET STARTED </button>
-                </div>
-                
-                <Modal isVisible={ isVisible } hide={ toggle } content={{ header: 'Group Form', body: <Form /> }}/>
+        <div className='container d-flex flex-column justify-content-center h-100'>
+            <div id='home-title' className='row justify-content-center mb-5' style={{fontSize: '5.5em'}}> 
+                <span style={{color: 'white'}}>Lets</span>
+                <span style={{color: '#E53A3A'}}>Watch</span>
             </div>
+            
+            <div className='row justify-content-center mt-3'>
+                <button id='home-button' className='main-button' onClick={ toggle } style={{backgroundColor: '#E53A3A', borderColor: '#E53A3A', color: 'white'}}> GET STARTED </button>
+            </div>
+            
+            <Modal isVisible={ isVisible } hide={ toggle } content={{ header: 'Group Form', body: <Form /> }}/>
         </div>
     );
 }
